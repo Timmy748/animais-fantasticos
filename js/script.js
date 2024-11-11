ab942b0db9fb145c1f4cbe5b6836a5a4fbf7183a
Timmy <timmychen748@gmail.com>
2024-11-10 21:32:18 -0300

    scroll suave transformado em uma classe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
-import initScrollSuave from "./modules/scroll-suave.js";
+import ScrollSuave from "./modules/scroll-suave.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/drop-down-menu.js";
@@ -10,11 +10,14 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFecthAnimais from "./modules/fecth-animais.js";
 import initFecthBitcoin from "./modules/fecth-bitcoin.js";
 
+
+const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#'")
+scrollSuave.init();
+
 initMenuMobile();
 initAnimacaoScroll();
 initTabNav();
 initAccordion();
-initScrollSuave();
 initModal();
 initTooltip();
 initDropdownMenu();
